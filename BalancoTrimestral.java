140afc570ac6f058439610f245939c9f70f68769
isabeels <101607831+isabeels@users.noreply.github.com>
2022-04-18 15:05:42 -0300

    Exercicio

---
+++ BalancoTrimestral.java
@@ -0,0 +1,16 @@
+package br.com.residencia;
+
+public class BalancoTrimestral {
+
+	public static void main(String[] args) {
+
+		int gastosJaneiro = 15000;
+		int gastosFevereiro = 23000;
+		int gastosMarco = 17000;
+		int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+		System.out.println("Gastos do Trimestre: " + gastosTrimestre);
+
+	}
+
+}
