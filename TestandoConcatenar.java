bdee83489dd133b2d5ae25b3a9426cc43151ee34
isabeels <101607831+isabeels@users.noreply.github.com>
2022-04-18 16:59:24 -0300

    Estudando formas de concatenar

---
+++ TestandoConcatenar.java
@@ -0,0 +1,15 @@
+package br.com.residencia;
+
+public class TestandoConcatenar {
+
+	public static void main(String[] args) {
+
+		String nome = "Isabel";
+		int idade = 25;
+		double renda = 10000;
+		
+		System.out.printf("%s tem %d anos e recebe R$ %.2f por mês como programadora.%n", nome, idade, renda);
+		//System.out.println(nome+" tem "+idade+" anos e recebe R$"+renda+" por mês como programadora.");
+	}
+
+}
