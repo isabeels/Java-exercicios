fb862ff67331aabe945a0cddad0e6c1a769eccb9
isabeels <101607831+isabeels@users.noreply.github.com>
2022-04-19 12:49:11 -0300

    Utilizando Getters e Setters

--- Caneta.java
+++ Caneta.java
@@ -2,34 +2,48 @@ package aulapratica;
 
 public class Caneta {
 
-	 String modelo;
-	 String cor;
-	 int carga;
-	 boolean tampada;
+	 public String modelo;
+	 public String cor;
+	 private float ponta;
+	 protected int carga;
+	 protected boolean tampada;
 	 
+	 
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public float getPonta() {
+		return ponta;
+	}
+	public void setPonta(float ponta) {
+		this.ponta = ponta;
+	}
+	public int getCarga() {
+		return carga;
+	}
+	public void setCarga(int carga) {
+		this.carga = carga;
+	}
+	
 	public void status() {
+		System.out.println("SOBRE A CANETA ");
+		System.out.printf("Modelo: %s%n", this.getModelo());
+		System.out.printf("Ponta: %.2f%n", this.getPonta());
+		System.out.printf("Carga: %d%n", this.getCarga());
+		System.out.printf("Cor: %s%n", this.getCor());
 		
-		System.out.println("Status da caneta:" + this.cor  + this.modelo  + this.tampada);
 	}
 	 
-	public void rabiscar() {
 	
-		if (this.tampada == true) {
-			System.out.println("Não pode rabiscar, está tampada.");
-		}
-			else {
-				System.out.println("Pode rabiscar a vontade!");
-			}
-	
-	}
-	
-	public void tampar() {
-	this.tampada = true;
-	}
-	
-	public void destampar() {
-	this.tampada = false;	
-	}
 	
 }
 
