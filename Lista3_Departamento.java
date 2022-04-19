9738dfb5358e35d4fd762b91ebae86316bc799c0
isabeels <101607831+isabeels@users.noreply.github.com>
2022-04-18 23:33:18 -0300

    Lista 3

---
+++ Lista3_Departamento.java
@@ -0,0 +1,37 @@
+package br.com.residencia;
+
+import java.util.Scanner;
+
+public class Lista3_Departamento {
+	public static void main (System[] args) {
+
+		int horasTrabalho;
+		double valorHora, vlBruto, vlLiquido, inss;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Quantidade de horas trabalhadas: ");
+		horasTrabalho = scan.nextInt();
+		System.out.println("Valor da hora de trabalho: ");
+		valorHora = scan.nextDouble();
+		scan.close();
+
+		vlBruto = horasTrabalho * valorHora;
+
+		if (vlBruto <= 1212) {
+			inss = 0.075;
+			vlLiquido = vlBruto - (vlBruto * inss);
+			System.out.println("Salário Bruto: "+ vlBruto);
+			System.out.println("Valor de desconto do INSS: " + inss);
+			System.out.println("Salário líquido: "+ vlLiquido);
+		} 
+		
+		else{
+			inss = 0.09;
+			vlLiquido = vlBruto - (vlBruto * inss);
+			System.out.println("Salário Bruto: "+ vlBruto);
+			System.out.println("Valor de desconto do INSS: " + inss);
+			System.out.println("Salário líquido: "+ vlLiquido);
+		}
+	}
+}
