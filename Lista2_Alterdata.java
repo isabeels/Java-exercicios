5c919a2f057deb383cb0d8979a733d4457930f71
isabeels <101607831+isabeels@users.noreply.github.com>
2022-04-18 23:30:20 -0300

    Lista 2

---
+++ Lista2_Alterdata.java
@@ -0,0 +1,17 @@
+/*Escrever o programa “Sobre a Empresa Alterdata”. Sua saída será a
+mensagem: “A Alterdata tem x anos.”, onde o x é a idade da empresa. A
+idade será atribuída em uma variável inteira.*/
+
+package br.com.residencia;
+
+public class Lista2_Alterdata {
+
+	public static void main(String[] args) {
+		
+		int idade = 33;
+		
+		System.out.println("A Alterdata tem " + idade + " anos!");
+
+	}
+
+}
