d2fbe70a93955afc95db93573dc925b1469b0d0c
isabeels <101607831+isabeels@users.noreply.github.com>
2022-04-18 23:32:34 -0300

    Lista 2

---
+++ Lista2_Universo.java
@@ -0,0 +1,12 @@
+package br.com.residencia;
+
+public class Lista2_Universo {
+
+	public static void main(String[] args) {
+
+		int universo = 42;
+
+		System.out.println("O valor do Universo é " + universo + "!");
+	}
+
+}
